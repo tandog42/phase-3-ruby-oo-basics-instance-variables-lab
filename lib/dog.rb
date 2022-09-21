0c5a894c22c05572f2466e18384c53289ac9c993
Tanner <troxs690@gmail.com>
2022-09-21 15:17:18 -0500

    d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  # setter method
+  def name=(value)
+    @name = value
+  end
 
+  # getter method
+  def name
+    @name
+  end
 end
+
+odie = Dog.new
+odie.name = "Odie"
+puts odie.name
